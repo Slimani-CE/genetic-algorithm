f9b8ab513efa52c67331b1e445d238f61c2f1a83
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2023-05-16 20:52:33 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # 🧬 Genetic Algorithm Implementation in Java
 ## Under development... 🚀
+
+## 📝 Table of Contents
+- [Overview](#overview)
+- [Genetic Algorithm Idea](#genetic-algorithm-idea)
+- [Genetic Algorithm Implementation](#genetic-algorithm-implementation)
+
+## 📖 Overview
+This project is a Java implementation of a Genetic Algorithm, a stochastic optimization algorithm inspired by natural selection and genetics. 
+
+## 🧬 Genetic Algorithm Idea
+The idea behind the Genetic Algorithm is to find the best solution to a problem by mimicking the process of natural selection and genetics. The algorithm starts with a population of individuals, each one representing a possible solution to the problem. Each individual is evaluated and assigned a fitness score, which represents how good the solution is. The individuals with the best fitness scores are selected to reproduce, and the offspring of the reproduction process will be the new population. The process is repeated until a solution is found or a maximum number of generations is reached.
+
+![ga-image](assets/ga-image.png)
+
+### 
+
+## 🧬 Genetic Algorithm Implementation
--- src/main/java/ma/enset/sma/sequential/AGApp.java
+++ src/main/java/ma/enset/sma/sequential/AGApp.java
@@ -3,7 +3,7 @@ package ma.enset.sma.sequential;
 public class AGApp {
     private static final int MAX_GENERATIONS = 5000;
     private static final int POPULATION_SIZE = 100;
-    private static final String TARGET = "mustaphaslimani jklwqjeioq i3209uqp2-";
+    private static final String TARGET = "Mustapha SLIMANI. Is one of the best developers in the world";
     public static void main(String[] args) {
         // Create an initial population
         Population population = new Population(POPULATION_SIZE, TARGET);
@@ -13,10 +13,10 @@ public class AGApp {
         // Perform evolution
         System.out.println("Evolution in progress...");
         for (int i = 0; i < MAX_GENERATIONS && population.getFirstFittest().getFitness() != 0; i++) {
+            population.calculateIndFitness();
             population.selection();
             population.crossover();
             population.mutation(0.5, Population.INSERTION_MUTATION);
-            population.calculateIndFitness();
             // Display fitness of the fittest individual
             System.out.println("Generation: " + (i + 1) + " (Fittest: " + population.getFirstFittest().getFitness() + ") Chromosome: " + population.getFirstFittest().getGenes().toString());
         }
